abe05a5d1d5298f2c8dcd536c7efcdef0ffb937b
kirito <1435462690@qq.com>
2026-04-19 17:36:56 +0800

    完成07_queue_maze和08_circular_queue练习

--- exercises/07_queue_maze/07_queue_maze.c
+++ exercises/07_queue_maze/07_queue_maze.c
@@ -2,6 +2,16 @@
 
 #define MAX_ROW 5
 #define MAX_COL 5
+#define MAX_QUEUE 100
+
+typedef struct {
+    int row, col;
+} Position;
+
+Position queue[MAX_QUEUE];
+int front = 0;
+int rear = 0;
+int size = 0;
 
 int maze[MAX_ROW][MAX_COL] = {
 	0, 1, 0, 0, 0,
@@ -11,10 +21,74 @@ int maze[MAX_ROW][MAX_COL] = {
 	0, 0, 0, 1, 0,
 };
 
+int visited[MAX_ROW][MAX_COL] = {0};
+Position prev[MAX_ROW][MAX_COL];
+
+int dirs[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+void enqueue(Position pos) {
+    queue[rear] = pos;
+    rear = (rear + 1) % MAX_QUEUE;
+    size++;
+}
+
+Position dequeue() {
+    Position pos = queue[front];
+    front = (front + 1) % MAX_QUEUE;
+    size--;
+    return pos;
+}
+
+int isEmpty() {
+    return size == 0;
+}
+
+int isFull() {
+    return size == MAX_QUEUE;
+}
+
 int main(void)
 {
-	// TODO: 在这里添加你的代码
-    // I AM NOT DONE
-
+    Position start = {0, 0};
+    Position end = {MAX_ROW - 1, MAX_COL - 1};
+    
+    enqueue(start);
+    visited[start.row][start.col] = 1;
+    prev[start.row][start.col] = (Position){-1, -1};
+    
+    while (!isEmpty()) {
+        Position current = dequeue();
+        
+        if (current.row == end.row && current.col == end.col) {
+            Position path[MAX_QUEUE];
+            int pathLen = 0;
+            Position p = current;
+            while (p.row != -1 && p.col != -1) {
+                path[pathLen++] = p;
+                p = prev[p.row][p.col];
+            }
+            for (int i = pathLen - 1; i >= 0; i--) {
+                printf("(%d, %d)\n", path[i].row, path[i].col);
+            }
+            return 0;
+        }
+        
+        for (int i = 0; i < 4; i++) {
+            int newRow = current.row + dirs[i][0];
+            int newCol = current.col + dirs[i][1];
+            
+            if (newRow >= 0 && newRow < MAX_ROW && 
+                newCol >= 0 && newCol < MAX_COL && 
+                maze[newRow][newCol] == 0 && 
+                visited[newRow][newCol] == 0) {
+                Position next = {newRow, newCol};
+                enqueue(next);
+                visited[newRow][newCol] = 1;
+                prev[newRow][newCol] = current;
+            }
+        }
+    }
+    
+    printf("No path!\n");
 	return 0;
 }
--- exercises/08_circular_queue/08_circular_queue.c
+++ exercises/08_circular_queue/08_circular_queue.c
@@ -14,13 +14,49 @@ typedef struct {
     int count;
 } Queue;
 
+void initQueue(Queue *q) {
+    q->head = 0;
+    q->tail = 0;
+    q->count = 0;
+}
+
+void enqueue(Queue *q, People p) {
+    q->data[q->tail] = p;
+    q->tail = (q->tail + 1) % MAX_PEOPLE;
+    q->count++;
+}
+
+People dequeue(Queue *q) {
+    People p = q->data[q->head];
+    q->head = (q->head + 1) % MAX_PEOPLE;
+    q->count--;
+    return p;
+}
+
+bool isEmpty(Queue *q) {
+    return q->count == 0;
+}
+
 int main() {
     Queue q;
     int total_people=50;
     int report_interval=5;
 
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    initQueue(&q);
+    
+    for (int i = 1; i <= total_people; i++) {
+        People p;
+        p.id = i;
+        enqueue(&q, p);
+    }
+    
+    while (q.count > 1) {
+        for (int i = 1; i < report_interval; i++) {
+            People p = dequeue(&q);
+            enqueue(&q, p);
+        }
+        dequeue(&q);
+    }
     
     printf("最后剩下的人是: %d\n", q.data[q.head].id);
 
