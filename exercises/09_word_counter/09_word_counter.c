c50adf11015264ffb5f951a9969a59f3b4c9b007
kirito <1435462690@qq.com>
2026-04-19 23:34:13 +0800

    完成 word_counter 和 my_strcpy 两个练习

--- exercises/09_word_counter/09_word_counter.c
+++ exercises/09_word_counter/09_word_counter.c
@@ -3,8 +3,20 @@
 int main() {
     char str[]="Don't ask what your country can do for you, but ask what you can do for your country.";
     int wordCount = 0;
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    int i = 0;
+    int inWord = 0;
+    
+    while (str[i] != '\0') {
+        if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z') || str[i] == '\'') {
+            if (!inWord) {
+                wordCount++;
+                inWord = 1;
+            }
+        } else {
+            inWord = 0;
+        }
+        i++;
+    }
     
     printf("单词数量: %d\n", wordCount);
     
