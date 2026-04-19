1bb2eaf83c0896f2eadd952c3cf1a9b6a3f07883
kirito <1435462690@qq.com>
2026-04-19 23:55:44 +0800

    完成 command_interpreter 和 student_management 两个练习

--- exercises/11_command_interpreter/11_command_interpreter.c
+++ exercises/11_command_interpreter/11_command_interpreter.c
@@ -57,11 +57,47 @@ int shell_parse(char *buf, char *argv[])
 {
     int argc = 0;
     int state = 0;
-    // TODO: 在这里添加你的代码，完成命令行解析
-    // 功能：将输入字符串buf按空格分割成多个参数，存入argv数组
-    // 返回：参数个数argc
-    // 提示：使用状态机的方式处理，注意处理字符串结束符
-    // I AM NOT DONE
+    int i = 0;
+    
+    while (buf[i] != '\0') {
+        switch (state) {
+            case 0:
+                if (buf[i] == ' ' || buf[i] == '\t') {
+                    i++;
+                } else if (buf[i] == '"') {
+                    state = 2;
+                    argv[argc++] = &buf[i + 1];
+                    i++;
+                } else {
+                    state = 1;
+                    argv[argc++] = &buf[i];
+                    i++;
+                }
+                break;
+            case 1:
+                if (buf[i] == ' ' || buf[i] == '\t') {
+                    buf[i] = '\0';
+                    state = 0;
+                    i++;
+                } else {
+                    i++;
+                }
+                break;
+            case 2:
+                if (buf[i] == '"') {
+                    buf[i] = '\0';
+                    state = 0;
+                    i++;
+                } else {
+                    i++;
+                }
+                break;
+        }
+        if (argc >= MAX_ARGS) {
+            break;
+        }
+    }
+    
     return argc;
 }
 
