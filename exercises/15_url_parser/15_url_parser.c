ffa6ca46b3e3d9bb645a824af68320f0a0b76890
kirito <1435462690@qq.com>
2026-04-23 23:29:03 +0800

    Fix url_parser implementation

--- exercises/15_url_parser/15_url_parser.c
+++ exercises/15_url_parser/15_url_parser.c
@@ -1,7 +1,7 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
 
 /**
  * URL参数解析器
@@ -9,53 +9,51 @@
  * 输出：解析出所有的key-value键值对，每行显示一个
  */
 
-int parse_url(const char* url) {
-    int err = 0;
+int parse_url(const char *url) {
+  int err = 0;
 
-    char* url_copy = strdup(url);
-    if (!url_copy) {
-        err = ENOMEM;
-        goto exit;
+  // 找到查询字符串的开始位置 '?'
+  const char *query_start = strchr(url, '?');
+  if (query_start == NULL) {
+    return 0;
+  }
+  query_start++; // 跳过 '?'
+
+  // 复制查询字符串以便使用 strtok 处理
+  size_t len = strlen(query_start);
+  char *query_copy = (char *)malloc(len + 1);
+  if (query_copy == NULL) {
+    return ENOMEM;
+  }
+  strcpy(query_copy, query_start);
+
+  
+  char *pair = strtok(query_copy, "&");
+  while (pair != NULL) {
+    // 使用 '=' 分割 key 和 value
+    char *sep = strchr(pair, '=');
+    if (sep != NULL) {
+      *sep = '\0';
+      char *key = pair;
+      char *value = sep + 1;
+      printf("key = %s, value = %s\n", key, value);
     }
+    pair = strtok(NULL, "&");
+  }
 
-    char* query = strchr(url_copy, '?');
-    if (!query) {
-        err = 1;
-        goto exit;
-    }
-    query++;
+  free(query_copy);
 
-    char* params = strdup(query);
-    if (!params) {
-        err = ENOMEM;
-        goto exit;
-    }
-
-    char* param = strtok(params, "&");
-    while (param != NULL) {
-        char* equal = strchr(param, '=');
-        if (equal) {
-            *equal = '\0';
-            char* key = param;
-            char* value = equal + 1;
-            printf("key = %s, value = %s\n", key, value);
-        }
-        param = strtok(NULL, "&");
-    }
-
-    free(params);
-exit:
-    free(url_copy);
-    return err;
+  return err;
 }
 
 int main() {
-    const char* test_url = "https://cn.bing.com/search?name=John&age=30&city=New+York";
+  const char *test_url =
+      "https://cn.bing.com/search?name=John&age=30&city=New+York";
 
-    printf("Parsing URL: %s\n", test_url);
-    printf("Parameters:\n");
+  printf("Parsing URL: %s\n", test_url);
+  printf("Parameters:\n");
 
-    parse_url(test_url);
+  parse_url(test_url);
 
-    return 0;
+  return 0;
 }
